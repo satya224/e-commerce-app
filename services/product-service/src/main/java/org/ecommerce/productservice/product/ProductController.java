93661be7df7d11740c76f3b274c2d56e264ac22c
satyaprakash <satyaprakash101010@gmail.com>
2024-11-24 15:23:11 +0530

    api-gateway implemented

--- services/product-service/src/main/java/org/ecommerce/productservice/product/ProductController.java
+++ services/product-service/src/main/java/org/ecommerce/productservice/product/ProductController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api/v1/products")
+@RestController
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
 
